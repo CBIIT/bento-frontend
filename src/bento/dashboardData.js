030f7869deeb52d6a137e8c55308ec9eb69121df
Ajay Doddapaneni <doddapaneni.ajay@gmail.com>
2020-06-23 10:38:17 -0400

    Making dashboard config into single file

---
+++ src/bento/dashboardData.js
@@ -0,0 +1,270 @@
+import gql from 'graphql-tag';
+
+export const dashboardData = {
+  stats: [
+    {
+      statTitle: 'Programs',
+      datatable_field: 'program',
+      type: 'field',
+      statAPI: 'numberOfPrograms',
+    },
+    {
+      statTitle: 'Studies',
+      datatable_field: 'study_acronym',
+      type: 'field',
+      statAPI: 'numberOfStudies',
+    },
+    {
+      statTitle: 'Subjects',
+      datatable_field: 'subject_id',
+      type: 'field',
+      statAPI: 'numberOfSubjects',
+    },
+    {
+      statTitle: 'samples',
+      type: 'array',
+      datatable_field: 'samples',
+      statAPI: 'numberOfSamples',
+    },
+    {
+      statTitle: 'Assays',
+      type: 'array',
+      datatable_field: 'lab_procedures',
+      statAPI: 'numberOfLabProcedures',
+    },
+    {
+      statTitle: 'files',
+      type: 'object',
+      datatable_field: 'files',
+      datatable_sub_field: 'file_id',
+      statAPI: 'numberOfFiles',
+    },
+  ],
+  facetSearch: [
+    {
+      group: 'Program', field: 'group', api: 'subjectCountByProgram', datafield: 'program', show: true,
+    },
+    {
+      group: 'Study', field: 'group', api: 'subjectCountByStudy', datafield: 'study_info', show: true,
+    },
+    {
+      group: 'Diagnosis', field: 'group', api: 'subjectCountByDiagnoses', datafield: 'diagnosis', show: true,
+    },
+    {
+      group: 'Recurrence Score', field: 'group', api: 'subjectCountByRecurrenceScore', datafield: 'recurrence_score', show: true,
+    },
+    {
+      group: 'Tumor Size', field: 'group', api: 'subjectCountByTumorSize', datafield: 'tumor_size', show: true,
+    },
+    {
+      group: 'Chemotherapy Regimen', field: 'group', api: 'subjectCountByChemotherapyRegimen', datafield: 'chemotherapy', show: true,
+    },
+    {
+      group: 'Tumor Grade', field: 'group', api: 'subjectCountByTumorGrade', datafield: 'tumor_grade', show: true,
+    },
+    {
+      group: 'ER Status', field: 'group', api: 'subjectCountByErStatus', datafield: 'er_status', show: true,
+    },
+    {
+      group: 'PR Status', field: 'group', api: 'subjectCountByPrStatus', datafield: 'pr_status', show: true,
+    },
+    {
+      group: 'Menopause Status', field: 'group', api: 'subjectCountByMenopauseStatus', datafield: 'menopause_status', show: true,
+    },
+  ],
+  widgets: [
+    {
+      type: 'sunburst',
+      label: 'Programs and Arms',
+      data: 'armsByTrial',
+      datatable_level1_field: 'program',
+      datatable_level2_field: 'study_acronym',
+      show: true,
+    },
+    {
+      type: 'donut',
+      label: 'Diagnosis',
+      data: 'caseCountByDisease',
+      datatable_field: 'diagnosis',
+      show: true,
+    },
+    {
+      type: 'donut',
+      label: 'Recurrence Score',
+      data: 'caseCountByGender',
+      datatable_field: 'recurrence_score',
+      show: true,
+    },
+    {
+      type: 'donut',
+      label: 'Tumor Size',
+      data: 'caseCountByRace',
+      datatable_field: 'tumor_size',
+      show: true,
+    },
+    {
+      type: 'donut',
+      label: 'Chemotherapy',
+      data: 'caseCountByEthnicity',
+      datatable_field: 'chemotherapy',
+      show: true,
+    },
+    {
+      type: 'donut',
+      label: 'Endocrine Therapy',
+      data: 'caseCountByPubmedId',
+      datatable_field: 'endocrine_therapy',
+      show: true,
+    },
+  ],
+  table: {
+    tableTitle: 'Cases',
+    tableData: [
+      {
+        field: 'subject_id',
+        label: 'Case ID',
+        sort: 'asc',
+        cellRenderPath: '/case',
+        primary: true,
+        display: true,
+      },
+      {
+        field: 'program',
+        label: 'Program Code',
+        sort: 'asc',
+        cellRenderPath: '/trial',
+        display: true,
+      },
+      {
+        field: 'study_acronym',
+        label: 'Arm',
+        sort: 'asc',
+        display: true,
+      },
+      {
+        field: 'diagnosis',
+        label: 'Diagnosis',
+        sort: 'asc',
+        display: true,
+      },
+      {
+        field: 'recurrence_score',
+        label: 'Recurrence Score',
+        sort: 'asc',
+        display: true,
+      },
+      {
+        field: 'tumor_size',
+        label: 'Tumor Size (cm)',
+        sort: 'asc',
+        display: true,
+      },
+      {
+        field: 'er_status',
+        label: 'ER Status',
+        sort: 'asc',
+        display: true,
+      },
+      {
+        field: 'pr_status',
+        label: 'PR Status',
+        sort: 'asc',
+        display: true,
+      },
+      {
+        field: 'age_at_index',
+        label: 'Age (years)',
+        sort: 'asc',
+        display: true,
+      },
+      {
+        field: 'survival_time',
+        label: 'Survival (days)',
+        sort: 'asc',
+        display: true,
+      },
+    ],
+  },
+};
+
+export const DASHBOARD_QUERY = gql`{
+  numberOfPrograms
+  numberOfStudies
+  numberOfSubjects
+  numberOfSamples
+  numberOfLabProcedures
+  numberOfFiles
+  subjectCountByProgram{
+        group
+         count
+      }
+    subjectCountByStudy{
+        group
+         count
+      }
+    subjectCountByDiagnoses{
+        group
+         count
+      }
+    subjectCountByRecurrenceScore{
+        group
+         count
+      }
+    subjectCountByTumorSize{
+        group
+         count
+      }
+    subjectCountByChemotherapyRegimen{
+        group
+         count
+      }
+    subjectCountByTumorGrade{
+        group
+         count
+      }
+  subjectCountByErStatus{
+        group
+         count
+      }
+  subjectCountByPrStatus{
+        group
+         count
+      }
+  subjectCountByMenopauseStatus{
+        group
+         count
+      }
+    
+    subjectOverView {
+      subject_id
+      study_info
+      samples
+      lab_procedures
+      program
+      study_acronym
+      study_short_description
+      diagnosis
+      recurrence_score
+      tumor_size
+      tumor_grade
+      er_status
+      pr_status
+      chemotherapy
+      endocrine_therapy
+      menopause_status
+      age_at_index
+      survival_time
+      survival_time_unit
+      files{
+        file_id
+        file_type
+        file_description
+        file_format
+        file_size
+        file_name
+        file_location
+        md5sum
+        file_status
+      }
+  }
+  }`;
--- src/pages/dashboard/caseTable/caseView.js
+++ src/pages/dashboard/caseTable/caseView.js
@@ -8,7 +8,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import MUIDataTable from 'mui-datatables';
 import Snackbar from '@material-ui/core/Snackbar';
 // import { Link } from 'react-router-dom';
-import dashboardTableData from '../../../bento/dashboardTable';
+import { dashboardData } from '../../../bento/dashboardData';
+
 import SuccessOutlinedIcon from '../../../utils/SuccessOutlined';
 import { cartSelectionMessages } from '../../../bento/cartWorkflowData';
 import CustomFooter from './customFooter';
@@ -82,7 +83,7 @@ const Cases = ({ classes, data }) => {
     saveButton.current.style.cursor = 'auto';
   });
 
-  const columns = dashboardTableData.tableData.map((column) => ({
+  const columns = dashboardData.table.tableData.map((column) => ({
     name: column.field,
     label: column.label,
     options: {
@@ -146,7 +147,7 @@ const Cases = ({ classes, data }) => {
         selectedRows.data[keyVlaue].index
       ));
       const selectedCaseId = selectedKeys.map((keyVlaue) => (
-        displayData[keyVlaue].data[dashboardTableData.tableData.findIndex(
+        displayData[keyVlaue].data[dashboardData.table.tableData.findIndex(
           (p) => p.primary === true,
         )].props.children
       ));
@@ -198,7 +199,7 @@ const Cases = ({ classes, data }) => {
 
         <Grid container>
           <Grid item xs={12} className={classes.caseTitle}>
-            {dashboardTableData.tableTitle}
+            {dashboardData.table.tableTitle}
           </Grid>
           <Grid item xs={12} id="table_cases">
             <MUIDataTable
