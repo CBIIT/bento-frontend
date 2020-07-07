2709d72a8491371a20914a0f1740e5511cb8ad02
skiran86 <skphp9@gmail.com>
2020-07-06 23:25:23 -0500

    updating anchor link

--- src/bento/landingData.js
+++ src/bento/landingData.js
@@ -37,7 +37,7 @@ export const landingPageData = {
     cardTitleText: 'About Your Data Commons',
     cardDescriptionText: 'Up to 460 Character Limitsed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in cumastera qui officia deserunt mollit anim id. Excepteur laborum.',
     cardCallToActionText: 'read more',
-    cardCallToActionLink: '/programs',
+    cardCallToActionLink: '/trials',
   },
   tile2: {
     alt: 'Alt tag1',
