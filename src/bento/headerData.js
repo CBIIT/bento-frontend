23c9a9bdde1f38086b4db314ac032ba2d0165f99
Ajay Doddapaneni <doddapaneni.ajay@gmail.com>
2020-08-17 16:44:40 -0400

    updating image dimensions

--- src/bento/headerData.js
+++ src/bento/headerData.js
@@ -1,7 +1,7 @@
 import Easter from '../assets/header/headerGraphic.png';
 
-// globalHeaderLogo image 468x80
-// globalHeaderImage: image 1200x60
+// globalHeaderLogo image 468x100
+// globalHeaderImage: image 2200x100
 export default {
   globalHeaderLogo: 'https://raw.githubusercontent.com/CBIIT/bento-tools/master/src/components/assets/header/CTDC_Logo.svg',
   globalHeaderLogoLink: 'https://frederick.cancer.gov/',
