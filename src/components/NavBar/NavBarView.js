2855dd7ad65bb804d2858264ce5c7c6130da78f2
Ajay Doddapaneni <doddapaneni.ajay@gmail.com>
2020-11-16 10:32:07 -0500

    adding cart count to navbar

--- src/components/NavBar/NavBarView.js
+++ src/components/NavBar/NavBarView.js
@@ -5,20 +5,13 @@ import {
   navBarData, navBarCartData, navBarstyling,
 } from '../../bento/navigationBarData';
 
-// const numberOfCases = useSelector((state) => {
-//   if (state.cart.fileIds) {
-//     return state.cart.fileIds.length;
-//   }
-//   return 0;
-// });
-
-const BentoNavBar = () => (
+const BentoNavBar = ({ cartFieldIds }) => (
   <>
     <NavBar
       navBarData={navBarData}
       navBarCartData={navBarCartData}
       navBarstyling={navBarstyling}
-      // numberOfCases={numberOfCases}
+      numberOfCases={cartFieldIds.length || 0}
     />
   </>
 );
