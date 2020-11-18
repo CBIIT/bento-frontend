bf6a51f066ea130ca5c6313612cf6f88bc4f2256
Ajay Doddapaneni <doddapaneni.ajay@gmail.com>
2020-11-18 11:16:46 -0500

    fixing package in dev

--- src/pages/dashboardTab/dashboardController.js
+++ src/pages/dashboardTab/dashboardController.js
@@ -3,14 +3,14 @@ import { connect } from 'react-redux';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Dashboard from './dashboard';
 import { fetchDataForDashboardTabDataTable } from './store/dashboardReducer';
-import { fetchDataForDashboardDataTable } from '../dashboard/dashboardState';
+// import { fetchDataForDashboardDataTable } from '../dashboard/dashboardState';
 import { Typography } from '../../components/Wrappers/Wrappers';
 
 class DashboardController extends Component {
   componentDidMount() {
-    const { dispatch } = this.props;
+    // const { dispatch } = this.props;
     fetchDataForDashboardTabDataTable();
-    dispatch(fetchDataForDashboardDataTable());
+    // dispatch(fetchDataForDashboardDataTable());
   }
 
   render() {
