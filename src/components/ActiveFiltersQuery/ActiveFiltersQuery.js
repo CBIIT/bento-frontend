333cb3d706498ba7fc37a6d32143a515e557732f
Chaparala Sri Kiran <skphp9@gmail.com>
2021-09-21 11:11:40 -0400

    ActiveFiltersQuery only on Filter Tab (#398)
    * ActiveFiltersQuery only on Filter Tab styles

--- src/components/ActiveFiltersQuery/ActiveFiltersQuery.js
+++ src/components/ActiveFiltersQuery/ActiveFiltersQuery.js
@@ -39,35 +39,36 @@ const ActiveFiltersQuery = ({ classes }) => {
     return '';
   });
 
+  const queryRenderer = () => {
+    let query = `<p class="${classes.query}">`;
+    activeFilters.forEach((filter, index) => {
+      query = `${query + (index !== 0 ? ' AND ' : '')}<span class="${classes.line}">${filter.filterName.toUpperCase()}</span>`;
+      query += (filter.checkbox.length === 1 ? ` IS <span class="${classes.line}">${filter.checkbox[0]}</span>` : ` IN (<span class="${classes.line}">${filter.checkbox.join(', ')}</span>)`);
+    });
+    query += '</p>';
+    [' AND ', ' IS ', ' IN '].forEach((opr) => {
+      query = query.replaceAll(opr, `<span class="${classes.operators}">${opr}</span>`);
+    });
+    return query;
+  };
+
   return (
-    <div>
-      {activeFilters.length > 0 ? (
+    <div className={classes.queryWrapper}>
+      <div>
         <div className={classes.clearQuery}>
           <Button
-            variant="contained"
             color="primary"
+            variant="outlined"
             className={classes.clearQueryButton}
             onClick={() => clearAllFilters()}
+            disabled={activeFilters.length <= 0}
           >
             Clear Query
           </Button>
         </div>
-      ) : ''}
-      <div className={classes.clearQueryResult}>
-        {activeFilters.map((filter, index) => (
-          <span>
-            <span>
-              {' '}
-              {index !== 0 ? 'AND ' : ''}
-              {filter.filterName}
-              {' '}
-            </span>
-            <span>
-              {' '}
-              {filter.checkbox.length === 1 ? `IS ${filter.checkbox[0]}` : `IN (${filter.checkbox.join()}) `}
-            </span>
-          </span>
-        ))}
+        <div className={classes.clearQueryResult}>
+          <div dangerouslySetInnerHTML={{ __html: queryRenderer() }} />
+        </div>
       </div>
     </div>
   );
@@ -75,18 +76,48 @@ const ActiveFiltersQuery = ({ classes }) => {
 
 const styles = (theme) => ({
   clearQueryButton: {
-    borderRadius: '8px',
+    borderRadius: '15px',
+    backgroundColor: '#fff',
     margin: '6px',
+  },
+  queryWrapper: {
+    padding: '10px 15px 0px 15px',
+    backgroundColor: '#f1f1f1',
+    height: '120px',
   },
   clearQuery: {
     float: 'left',
     paddingTop: '15px',
   },
+  query: {
+    fontFamily: 'Nunito Sans Black',
+    fontSize: '18px',
+    letterSpacing: '1px',
+    marginRight: '100px',
+    lineHeight: 1.65,
+  },
+  line: {
+    paddingBottom: '3px',
+    borderBottomStyle: 'solid',
+    borderBottomWidth: '2px',
+    borderBottomColor: '#10A075',
+    width: 'fit-content',
+  },
+  operators: {
+    color: '#64ACD5',
+    marginLeft: '3px',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    marginRight: '3px',
+    borderBottom: 'none',
+    fontFamily: 'Nunito Sans Black',
+    textDecoration: 'none',
+  },
   clearQueryResult: {
-    padding: '20px 20px 0px 15px',
-    height: '80px',
+    height: '120px',
     overflowY: 'auto',
-    marginRight: '250px',
+    marginTop: '-20px',
+    marginLeft: '164px',
     color: theme.palette.clearQueryResultColor.color,
   },
 });
