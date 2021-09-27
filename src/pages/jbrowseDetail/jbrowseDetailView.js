5baceaa1d15de9bd22836abb1d523617e0a5cfb2
skiran86 <skphp9@gmail.com>
2021-09-27 10:34:44 -0400

    Pass bam and bai file location dynamically

--- src/pages/jbrowseDetail/jbrowseDetailView.js
+++ src/pages/jbrowseDetail/jbrowseDetailView.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import {
@@ -38,8 +37,8 @@ const tracks = [
     type: 'AlignmentsTrack',
     adapter: {
       type: 'BamAdapter',
-      bamLocation: { uri: 'https://d1c9wxtw4a60lg.cloudfront.net/PACT1-1K/wgEncodeUwRepliSeqBg02esG1bAlnRep1.bam?Expires=1632576488&Key-Pair-Id=K3USBVWL2TSK7R&Signature=cjNFSJZUZssSIjepwsGVFg5FLSsEe9PPG2Grfeod-vHmWzAacUEQdMIyWXP8SOzierPAtD1p8JfM7-qhvbP-bkD-2b1YMT1VCJlLDGaxLnWB3Oyes~ZZCCszw0MkMpoA78paGQ127E6NcbSle1Vu6JzMp3Ukl~pJrzPgJKUAyYkRVyttOfKWHmk4zNYRqCNW0-Sdh0E6cJo4RBnGj36EVy-yB2UiVbeNfBYeRsutSRQBDYf7nQD4SEtLPo80eVP6q-LHqj0tlq40LZ1Q3SKRmk9s~8SoxeXqal5-s-Rjqse6aHT8eCbYbc9-QxltBsruXTLJKUmMm2qVhlRGo5wovA__' },
-      index: { location: { uri: 'https://d1c9wxtw4a60lg.cloudfront.net/PACT1-1K/wgEncodeUwRepliSeqBg02esG1bAlnRep1.bam.bai?Expires=1632576499&Key-Pair-Id=K3USBVWL2TSK7R&Signature=fbsqCGdxfa-ezAoEr9N6lelBBjZCWJNuJToCG5VU-C~-sHALO4gqP-SxmBtRhnkrKgzO1cNv3BS-E~~jcA9xkC8Mt0qJfCmuAHXYmydzAeY5fzjmowJRX-EUKicY9lpPNODVc0OwHj4JDUTkmxlNae8LrxvYOPd1a~skabRrfg~hEoLZPVAitHdJ9T1KeTUTC~AeKegtgAQ7hcmDdCqATx~C-R87FWkbPmnrCvmaqDAFA94KmLgn9CtRKpacr2geR8W29J2mjQW~~1zZhKPWYajYua2r1dMBBzi5ppUMJYmOkmpDtRww2dRLgKnEZR9Ia4Y8S4OOAm-M5Hkv2csHFg__' } },
+      bamLocation: { uri: '' },
+      index: { location: { uri: '' } },
     },
   },
   {
@@ -55,36 +54,52 @@ const tracks = [
   },
 ];
 
-const state = createViewState({
-  assembly,
-  tracks,
-  location: '10:29,838,737..29,838,819',
-});
-
 const JBrowseDetail = ({ bamFiles }) => {
-  // eslint-disable-next-line
   const [trackList, setTracks] = useState(tracks);
 
-  const getFileUris = async () => {
-    const resp1 = await axios.get(bamFiles[0].file_location);
-    const resp2 = await axios.get(bamFiles[0].file_location, {
-      headers: {
-        'Content-Type': 'applciation/json',
-      },
+  const getState = () => {
+    const state = createViewState({
+      assembly,
+      tracks: trackList,
+      location: '10:29,838,737..29,838,819',
     });
-    console.log(resp1, resp2, 'SOMETHING');
+    return state;
+  };
+
+  const configureAdapter = () => {
+    const adapter = {
+      type: 'BamAdapter',
+      bamLocation: { uri: '' },
+      index: { location: { uri: '' } },
+    };
+
+    bamFiles.forEach((file) => {
+      if (file.file_type === 'bam') {
+        adapter.bamLocation.uri = file.file_location;
+      }
+      if (file.file_type === 'bai') {
+        adapter.index.location.uri = file.file_location;
+      }
+    });
+
+    let alignmentTrack = trackList[0];
+    alignmentTrack = {
+      ...alignmentTrack,
+      adapter,
+    };
+
+    setTracks([alignmentTrack, trackList[1]]);
   };
 
   useEffect(() => {
-    // trackList[0].adapter.bamLocation.uri =
     if (bamFiles.length > 0) {
-      getFileUris();
+      configureAdapter();
     }
   }, [bamFiles]);
 
   return (
     <ThemeProvider theme={theme}>
-      <JBrowseLinearGenomeView viewState={state} />
+      <JBrowseLinearGenomeView viewState={getState()} />
     </ThemeProvider>
   );
 };
