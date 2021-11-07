d3130afff1f69b8d0b15d72e25e3c2c4ad0cb13c
Ajay Doddapaneni <doddapaneni.ajay@gmail.com>
2021-11-06 23:34:46 -0400

    adding onkey down enter for search autocomplete

--- src/components/Search/searchAutoFillComponent.js
+++ src/components/Search/searchAutoFillComponent.js
@@ -1,19 +1,18 @@
-/* eslint-disable */
 import React from 'react';
 import {
   TextField, CircularProgress, withStyles,
 } from '@material-ui/core';
-import { Redirect } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { getSearch } from '../../pages/dashboardTab/store/dashboardReducer';
 
 function searchComponent() {
-  const [open, setOpen] = React.useState(false);
-  const [sendToSearchPage, setSendToSearchPage] = React.useState(false);
+  const history = useHistory();
+
+  const [open] = React.useState(false);
   const [inputValue, setInputValue] = React.useState('');
   const [options, setOptions] = React.useState([]);
   const loading = open;
-  const [value, setValue] = React.useState([]);
 
   React.useEffect(() => {
     if (!open) {
@@ -22,56 +21,63 @@ function searchComponent() {
   }, [open]);
 
   function onChange(newValue = []) {
-    setValue(newValue);
-    setSendToSearchPage(true);
+    const path = `/search/${newValue[0]}`;
+    history.push(path);
   }
 
   async function getAutoCompleteRes(newValue = []) {
     setInputValue(newValue);
     const searchResp = await getSearch(newValue);
+    // This needs to be replaced as obj.keys but __typename param coming extra from BE
     const keys = ['program_ids', 'arm_ids', 'subject_ids', 'sample_ids', 'file_ids'];
-    const mapOption = keys.map((key) => searchResp[key].map((id) => ({ type: key, title: id })));
+    const mapOption = keys.map((key) => searchResp[key].map((id) => (id)));
     const option = mapOption.reduce((acc = [], iterator) => [...acc, ...iterator]);
     setOptions(option);
   }
 
   return (
     <>
-      {sendToSearchPage ? <Redirect to={`/search/${value[0].title}`} />
-        : (
-          <Autocomplete
-            freeSolo
-            id="search"
-            onChange={(event, newValue) => onChange(newValue)}
-            multiple
-            inputValue={inputValue}
-            onInputChange={(event, newInputValue) => {
-              getAutoCompleteRes(newInputValue);
+      <Autocomplete
+        freeSolo
+        id="search"
+        onChange={(event, newValue) => onChange(newValue)}
+        multiple
+        inputValue={inputValue}
+        onInputChange={(event, newInputValue) => {
+          getAutoCompleteRes(newInputValue);
+        }}
+        style={{ width: 300 }}
+        filterOptions={(optionSlice) => optionSlice.slice(0, 5)}
+        getOptionLabel={(option) => option}
+        options={options}
+        loading={loading}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="search"
+            variant="filled"
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' && e.target.value) {
+                //   setValue([{
+                //     type: "subject_ids",
+                //     title: e.target.value
+                // }]);
+                // setSendToSearchPage(true);
+                onChange([e.target.value]);
+              }
             }}
-            value={value}
-            style={{ width: 300 }}
-            filterOptions={(optionSlice) => optionSlice.slice(0, 5)}
-            getOptionLabel={(option) => option.title}
-            options={options}
-            loading={loading}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label="search"
-                variant="filled"
-                InputProps={{
-                  ...params.InputProps,
-                  endAdornment: (
-                    <>
-                      {loading ? <CircularProgress color="inherit" size={20} /> : null}
-                      {params.InputProps.endAdornment}
-                    </>
-                  ),
-                }}
-              />
-            )}
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <>
+                  {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                  {params.InputProps.endAdornment}
+                </>
+              ),
+            }}
           />
         )}
+      />
     </>
   );
 }
