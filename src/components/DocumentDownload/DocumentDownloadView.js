69ba814e6e98bf55b682ad0c97fe2607e436e986
Karan Sheth <karan.sheth60@gmail.com>
2022-03-14 12:42:04 -0400

    Added session timeout & modal window.

--- src/components/DocumentDownload/DocumentDownloadView.js
+++ src/components/DocumentDownload/DocumentDownloadView.js
@@ -10,18 +10,29 @@ import CustomIcon from '../CustomIcon/CustomIconView';
 import { jBrowseOptions } from '../../bento/jbrowseDetailData';
 import { useGoogleAuth } from '../GoogleAuth/GoogleAuthProvider';
 import globalData from '../../bento/siteWideConfig';
+import SessionTimeOutModal from '../sessionTimeOutModal';
 
 const FILE_SERVICE_API = env.REACT_APP_FILE_SERVICE_API;
 
-const fetchFileToDownload = (fileURL = '') => {
+const fetchFileToDownload = (fileURL = '', signOut, setShowModal) => {
   fetch(`${FILE_SERVICE_API}${fileURL}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/pdf',
     },
   })
-    .then((response) => response.text())
-    .then((filePath) => {
+    .then((response) => {
+      if (response.status === 403) {
+        // sign out && open modal
+        signOut();
+        setShowModal(true);
+        return '';
+      }
+      return response.text();
+    }).then((filePath) => {
+      if (filePath === '') {
+        return;
+      }
       // Create blob link to download
       const link = document.createElement('a');
       link.href = filePath;
@@ -55,41 +66,56 @@ const DocumentDownload = ({
 }) => {
   const {
     signIn,
+    signOut,
   } = useGoogleAuth();
   const isSignedIn = useSelector((state) => state.login.isSignedIn);
+  const [showModal, setShowModal] = React.useState(false);
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
 
   return (
     <>
-      { (globalData.enableAuthentication && !isSignedIn) ? (
-        <ToolTip classes={{ tooltip: classes.customTooltip, arrow: classes.customArrow }} title={toolTipTextUnauthenticated} arrow placement="bottom">
-          <div
-            style={{ textAlign: 'center' }}
-            onClick={() => signIn()}
-          >
-            <CustomIcon imgSrc={iconUnauthenticated} />
-          </div>
-        </ToolTip>
-      ) : (fileFormat === 'bam' || fileFormat === 'bai') && jBrowseOptions.jBrowse ? (
-        <ToolTip classes={{ tooltip: classes.customTooltip, arrow: classes.customArrow }} title={toolTipTextFileViewer} arrow placement="bottom">
-          <Link
-            to={`/fileViewer/${caseId}`}
-          >
-            <CustomIcon imgSrc={iconFileViewer} />
-          </Link>
-        </ToolTip>
-      ) : fileSize < maxFileSize ? (
-        <ToolTip classes={{ tooltip: classes.customTooltip, arrow: classes.customArrow }} title={toolTipTextFileDownload} arrow placement="bottom">
-          <div onClick={() => fetchFileToDownload(fileLocation)}>
-            <CustomIcon imgSrc={iconFileDownload} />
-          </div>
-        </ToolTip>
-      ) : (
-        <ToolTip classes={{ tooltip: classes.customTooltip, arrow: classes.customArrow }} title={toolTipTextFilePreview} arrow placement="bottom">
-          <span>
-            <CustomIcon imgSrc={iconFilePreview} />
-          </span>
-        </ToolTip>
-      )}
+      <div>
+        { (globalData.enableAuthentication && !isSignedIn) ? (
+          <ToolTip classes={{ tooltip: classes.customTooltip, arrow: classes.customArrow }} title={toolTipTextUnauthenticated} arrow placement="bottom">
+            <div
+              style={{ textAlign: 'center' }}
+              onClick={() => signIn()}
+            >
+              <CustomIcon imgSrc={iconUnauthenticated} />
+            </div>
+          </ToolTip>
+        ) : (fileFormat === 'bam' || fileFormat === 'bai') && jBrowseOptions.jBrowse ? (
+          <ToolTip classes={{ tooltip: classes.customTooltip, arrow: classes.customArrow }} title={toolTipTextFileViewer} arrow placement="bottom">
+            <Link
+              to={`/fileViewer/${caseId}`}
+            >
+              <CustomIcon imgSrc={iconFileViewer} />
+            </Link>
+          </ToolTip>
+        ) : fileSize < maxFileSize ? (
+          <ToolTip classes={{ tooltip: classes.customTooltip, arrow: classes.customArrow }} title={toolTipTextFileDownload} arrow placement="bottom">
+            <div onClick={() => fetchFileToDownload(fileLocation, signOut, setShowModal)}>
+              <CustomIcon imgSrc={iconFileDownload} />
+            </div>
+          </ToolTip>
+        ) : (
+          <ToolTip classes={{ tooltip: classes.customTooltip, arrow: classes.customArrow }} title={toolTipTextFilePreview} arrow placement="bottom">
+            <span>
+              <CustomIcon imgSrc={iconFilePreview} />
+            </span>
+          </ToolTip>
+        )}
+        <SessionTimeOutModal
+          open={showModal}
+          closeModal={closeModal}
+          handleClose={closeModal}
+          submit={signIn}
+          message="Please login to access files!"
+        />
+      </div>
     </>
   );
 };
