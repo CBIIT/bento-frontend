2592f1b52eac0da039739a3a2d8a74ff456f41a7
Ajay Doddapaneni <doddapaneni.ajay@gmail.com>
2022-03-16 12:13:44 -0400

    diasble auth for testing

--- src/bento/siteWideConfig.js
+++ src/bento/siteWideConfig.js
@@ -2,7 +2,7 @@ export default {
   // Suggested for replaceEmptyValueWith: 'N/A' or '-' or ''
   replaceEmptyValueWith: '',
   // Enable authenication
-  enableAuthentication: true,
+  enableAuthentication: false,
   // List for options for authentication empty array defaults to google
   authEndPoint: ['google'], // authEndPoint: []
 };
