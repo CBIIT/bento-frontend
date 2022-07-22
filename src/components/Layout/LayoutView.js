b64304b7986d6124b8ac04dc1f32e7c6bb814dab
Nahom Tesfatsion <tesfatsion.nahom@nih.gov>
2022-07-22 09:41:48 -0400

    add route

--- src/components/Layout/LayoutView.js
+++ src/components/Layout/LayoutView.js
@@ -23,6 +23,7 @@ import JBrowseDetail from '../../pages/jbrowseDetail/jbrowseDetailController';
 import GlobalSearch from '../../pages/search/searchView';
 import GlobalSearchController from '../../pages/search/searchViewController';
 import adminController from '../../pages/admin/adminController';
+import reviewRequestController from '../../pages/admin/reviewPendingDAR/reviewRequestController';
 import Login from '../../pages/accessManagment/login';
 import userRegistration from '../../pages/accessManagment/userRegistration';
 
@@ -65,6 +66,7 @@ const Layout = ({ classes, isSidebarOpened }) => (
             <PrivateRoute path="/search/:id" component={GlobalSearchController} />
             <PrivateRoute path="/fileViewer/:id" component={JBrowseDetail} />
             <Route path="/admin" component={adminController} />
+            <Route path="/review/:id" component={reviewRequestController} />
             {/* END: Private Routes */}
 
             {aboutPageRoutes.map(
