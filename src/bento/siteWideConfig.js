8ea6efa2b7aa54c56cbfeefdb9f690c3f2fbb63c
ogwuazors <95305471+ogwuazors@users.noreply.github.com>
2022-08-30 09:43:14 -0400

    Limiting node level to 30 characters.

--- src/bento/siteWideConfig.js
+++ src/bento/siteWideConfig.js
@@ -16,6 +16,11 @@ export const userProfileRoute = '/profile';
 export const PUBLIC_ACCESS = 'none';
 
 // Node level access
-export const NODE_LEVEL_ACCESS = true;
+export const NODE_LEVEL_ACCESS = false;
 export const NODE_NAME = 'Arm';
-export const NODE_LABEL = 'Study Arm(s)';
+export const NODE_LABEL = () => {
+  const labelLimit = 30;
+  const label = 'Study Arm(s)';
+
+  return label.substring(0, labelLimit);
+};
