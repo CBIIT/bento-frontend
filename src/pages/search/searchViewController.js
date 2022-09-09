85c7506b6194baabf4b0d0d3704cb5354fd79dab
ogwuazors <95305471+ogwuazors@users.noreply.github.com>
2022-09-09 13:40:29 -0400

    ACL check is breaking build

--- src/pages/search/searchViewController.js
+++ src/pages/search/searchViewController.js
@@ -8,7 +8,7 @@ const SearchViewContainer = ({ match }) => {
   const isSignedIn = useSelector((state) => state.login.isSignedIn);
   const isAdmin = useSelector((state) => state.login && state.login.role && state.login.role === 'admin');
   const hasApprovedArms = useSelector(
-    (state) => state.login.acl.some((arm) => arm.accessStatus === 'approved'),
+    (state) => state.login.acl && state.login.acl.some((arm) => arm.accessStatus === 'approved'),
   );
   const isAuthorized = isSignedIn && (hasApprovedArms || isAdmin);
 
