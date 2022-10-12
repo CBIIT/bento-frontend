542872c628b5b911d25a67514356d0919cb1d6f8
Karan Sheth <karan.sheth60@gmail.com>
2022-10-12 11:42:32 -0400

    Cart button will show "0" if user not logged in and A&A enabled. Added requiredSiginIn flag for cart.

--- src/components/NavBar/NavBarView.js
+++ src/components/NavBar/NavBarView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavBar } from 'bento-components';
 import {
   navBarData, navBarCartData, navBarstyling,
@@ -6,24 +7,33 @@ import {
 import Login from '../Auth/loginComponent';
 import globalData from '../../bento/siteWideConfig';
 
-const BentoNavBar = ({ cartFieldIds }) => (
-  <>
-    {globalData.enableAuthentication ? (
+const BentoNavBar = ({ cartFieldIds = [] }) => {
+  const { enableAuthentication } = globalData;
+  const isSignedIn = useSelector((state) => state.login.isSignedIn);
+
+  const getNumberOfCase = () => {
+    const { length: numberOfCases } = cartFieldIds;
+
+    if (!enableAuthentication) return numberOfCases;
+
+    return isSignedIn ? numberOfCases : 0;
+  };
+
+  const getLoginComponent = () => {
+    if (!enableAuthentication) return undefined;
+    return Login;
+  };
+
+  return (
+    <>
       <NavBar
         navBarData={navBarData}
         navBarCartData={navBarCartData}
         navBarstyling={navBarstyling}
-        numberOfCases={cartFieldIds.length || 0}
-        LoginComponent={Login}
+        numberOfCases={getNumberOfCase()}
+        LoginComponent={getLoginComponent()}
       />
-    ) : (
-      <NavBar
-        navBarData={navBarData}
-        navBarCartData={navBarCartData}
-        navBarstyling={navBarstyling}
-        numberOfCases={cartFieldIds.length || 0}
-      />
-    ) }
-  </>
-);
+    </>
+  );
+};
 export default BentoNavBar;
