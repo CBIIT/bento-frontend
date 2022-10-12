b8142c4a65e2baa30cc8d838adbe00fa2c6a0261
Karan Sheth <karan.sheth60@gmail.com>
2022-10-12 09:32:12 -0400

    fixes for Bento 2107.

--- src/bento/userLoginData.js
+++ src/bento/userLoginData.js
@@ -4,17 +4,17 @@ export const loginProvidersData = {
   google: {
     key: 'google',
     icon: 'https://raw.githubusercontent.com/CBIIT/datacommons-assets/main/bento/images/icons/png/google.png',
-    loginButtonText: 'Sign in with Google',
+    loginButtonText: 'Google',
   },
   loginGov: {
     key: 'loginGov',
     icon: 'https://raw.githubusercontent.com/CBIIT/datacommons-assets/main/bento/images/icons/png/login.gov.png',
-    loginButtonText: 'Sign in Login.gov',
+    loginButtonText: 'Login.gov',
   },
   nih: {
     key: 'nih',
     icon: 'https://raw.githubusercontent.com/CBIIT/datacommons-assets/main/bento/images/icons/png/nih_itrust.png',
-    loginButtonText: 'Sign in NIH iTrust',
+    loginButtonText: 'NIH iTrust',
   },
 };
 
