8b9bd243c40d71a132725fcf88bb70c84ff88a2e
Alec Mattu <alec.mattu@nih.gov>
2023-02-13 11:59:18 -0500

    BENTO-2228 Extract and refactor Widget and Charts

---
+++ src/bento-core/Widgets/index.js
@@ -0,0 +1,9 @@
+export {
+  DonutChartGenerator, DEFAULT_CONFIG_DONUT, DEFAULT_COLORS_EVEN, DEFAULT_COLORS_ODD,
+} from './DonutChart/DonutChartGenerator';
+export {
+  SunburstChartGenerator, DEFAULT_CONFIG_SUNBURST,
+} from './SunburstChart/SunburstChartGenerator';
+export {
+  WidgetGenerator, DEFAULT_CONFIG_WIDGET,
+} from './WidgetGenerator';
