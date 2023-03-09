ffe40a800975b7055584f0972fbe684d89a04bbc
rana22 <ambar.rana123@gmail.com>
2023-03-09 12:02:59 -0500

    move paginated table to bento core

---
+++ src/pages/dashTemplate/tabs/table/CustomColumnConfig.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { cellTypes, headerTypes } from '../../../../bento-core/Table/util/Types';
+import DocumentDownloadView from '../../../../components/DocumentDownload/DocumentDownloadView';
+
+export const CustomCellView = (props) => {
+  const { downloadDocument, documentDownloadProps } = props;
+  if (downloadDocument) {
+    return (
+      <DocumentDownloadView
+        fileSize={props.file_size}
+        caseId={props[documentDownloadProps.caseIdColumn]}
+        fileFormat={props[documentDownloadProps.fileFormatColumn]}
+        fileLocation={props[documentDownloadProps.fileLocationColumn]}
+        {...documentDownloadProps}
+      />
+    );
+  }
+  // other custom elem
+  return (<></>);
+};
+
+export const CustomHeaderCellView = () => (<></>);
+
+/**
+* set column configuration
+* @param {*} columns
+* @returns config columns
+*/
+export const configColumn = (columns) => {
+  /**
+  * display columns as configuration
+  * set custom cell render for column
+  */
+  const displayColumns = columns.filter((col) => col.display);
+  const displayCustomView = [...displayColumns].map((column) => {
+    if (column.cellType === cellTypes.CUSTOM_ELEM) {
+      return {
+        ...column,
+        customCellRender: (props) => <CustomCellView {...props} />,
+      };
+    }
+    return column;
+  });
+
+  /**
+  * custom header view configuration
+  */
+  const displayCustomHeader = [...displayCustomView].map((column) => {
+    if (column.headerType === headerTypes.CUSTOM_ELEM) {
+      return {
+        ...column,
+        customColHeaderRender: (props) => <CustomHeaderCellView {...props} />,
+      };
+    }
+    return column;
+  });
+  return displayCustomHeader;
+};
+
+export const cofig = {};
