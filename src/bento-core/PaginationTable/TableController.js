4ca45daef3eb2dde9bc2205987a1357f4e4797ec
rana22 <ambar.rana123@gmail.com>
2023-03-15 15:29:06 -0400

    add comments

--- src/bento-core/PaginationTable/TableController.js
+++ src/bento-core/PaginationTable/TableController.js
@@ -3,8 +3,12 @@ import { CircularProgress, Container } from '@material-ui/core';
 import TableView from '../Table/TableView';
 import { getTableData, setSelectedRows } from './TableService';
 
+/**
+* Updates table row when table state is changed 1. (paginated action) and 2. filter action
+*/
 const TableController = ((props) => {
-  const { tableData } = getTableData(props);
+  const { activeFilters, table } = props;
+  const { tableData } = getTableData({ activeFilters, table });
   if (!tableData) {
     return (
       <Container maxWidth="xl">
@@ -13,9 +17,9 @@ const TableController = ((props) => {
     );
   }
   /**
-  * set selected row
+  * update rows
+  * set selected rows isChecked to TRUE
   */
-  const { table } = props;
   const updateRows = setSelectedRows(tableData, table);
   return (
     <>
