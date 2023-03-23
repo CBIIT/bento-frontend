80aaa39facabc2dea58d3f6a6e6a16939119c912
Ankerso <ankersosed@nci-02277705-ml.myfiosgateway.com>
2023-03-23 16:29:01 -0400

    Finished tab and integrated it with dashboard

--- src/pages/dashTemplate/DashTemplateView.js
+++ src/pages/dashTemplate/DashTemplateView.js
@@ -1,42 +1,47 @@
 import React from 'react';
-import { CircularProgress, withStyles } from '@material-ui/core';
+import { withStyles } from '@material-ui/core';
 import styles from './DashTemplateStyle';
 import BentoFacetFilter from './sideBar/BentoFacetFilter';
 import WidgetView from './widget/WidgetView';
 import StatsView from '../../components/Stats/StatsView';
-import TabsView from './tabs/TabController';
+import Tabs from '../../bento-core/Tab/Tabs';
 
 const DashTemplate = ({
   classes,
   dashData,
-  activeFilters,
-}) => {
-  if (!dashData) {
-    return (<CircularProgress />);
-  }
-  return (
-    <div className={classes.dashboardContainer}>
-      <StatsView data={dashData} />
-      <div>
-        <div className={classes.content}>
-          <div className={classes.sideBar}>
-            <BentoFacetFilter searchData={dashData} />
-          </div>
-          <div className={classes.rightContent}>
-            <div className={classes.widgetsContainer}>
-              <WidgetView
-                data={dashData}
-              />
-              <TabsView
-                dashboardStats={dashData}
-                activeFilters={activeFilters}
-              />
-            </div>
+  onTabChange,
+  tabData,
+}) => (
+  <div className={classes.dashboardContainer}>
+    <StatsView data={dashData} />
+    <div>
+      <div className={classes.content}>
+        <div className={classes.sideBar}>
+          <BentoFacetFilter searchData={dashData} />
+        </div>
+        <div className={classes.rightContent}>
+          <div className={classes.widgetsContainer}>
+            <WidgetView
+              data={dashData}
+            />
+            <Tabs
+              tabData={tabData}
+              onChange={onTabChange}
+              styles={{
+                tabsLabel:
+                  {
+                    '& .count': {
+                      marginLeft: '5px',
+                      fontSize: '17px',
+                    },
+                  },
+              }}
+            />
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default withStyles(styles)(DashTemplate);
