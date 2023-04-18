b940124208ab6fd06a6c19fdf09452aaf86ac913
rana22 <ambar.rana123@gmail.com>
2023-04-18 10:30:56 -0400

    remove unused codes

--- packages/tab/src/Tabs.js
+++ packages/tab/src/Tabs.js
@@ -1,86 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Tab,
   Tabs,
   createTheme, 
   ThemeProvider,
 } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import generateStyle from './utils/generateStyle';
-import TabThemeProvider from './TabThemeProvider';
 import { defaultTheme } from './defaultTheme';
-
-/* Tabs coponenent */
-// function Tabs({
-//   tabData,
-//   onChange,
-//   styles,
-//   themes,
-//   overrides
-// }) {
-//   /* styles */
-//   const generatedStyle = generateStyle(styles);
-//   const useStyles = makeStyles(generatedStyle);
-//   const classes = useStyles();
-
-
-//   /* tab headers and specific style generator */
-//   const getSpecificTabStyle = (currentSelectedTab, index, tabHeaderStyle = undefined) => {
-//     let style = {};
-//     if (tabHeaderStyle) {
-//       style = tabHeaderStyle.root ? { ...tabHeaderStyle.root } : {};
-//       if (currentSelectedTab === index && tabHeaderStyle.selected) {
-//         style = { ...style, ...tabHeaderStyle.selected };
-//       }
-//     }
-//     return style;
-//   };
-
-//   const TabHeader = tabData.map((tab, index) => {
-//     const specificTabStyle = getSpecificTabStyle(currentTab, index, tab.label.style);
-//     return (
-//       <MuiTab
-//         key={index}
-//         className={classes.tabsLabel}
-//         label={tab.label.content}
-//         style={specificTabStyle}
-//         disableRipple
-//       />
-//     );
-//   });
-
-//   /* event handlers */
-//   const handleChange = (event, newValue) => {
-//     setCurrentTab(newValue);
-
-//     if (onChange) {
-//       onChange(newValue);
-//     }
-//   };
-
-//   return (
-//     <div className={classes.tabsContainer}>
-//       <TabThemeProvider
-//         muiTabTheme={generatedStyle.muiTab}
-//         muiTabsTheme={generatedStyle.muiTabs}
-//         themes={themes}
-//         overrides={overrides}
-//       >
-//         <MuiTabs value={currentTab} onChange={handleChange}>
-//           {TabHeader}
-//         </MuiTabs>
-
-//         {tabData.map((tab, index) => (
-//           <Typography key={index} component="div" className={classes.tabsPanel} hidden={currentTab !== index}>
-//             {tab.panel}
-//           </Typography>
-//         ))}
-
-//       </TabThemeProvider>
-//     </div>
-//   );
-// }
-
-// export default Tabs;
 
 const TabItems = ({
   tabItems,
@@ -103,12 +27,6 @@ const TabItems = ({
       </span>
     </>
   );
-
-  /* Component states */
-  // const [currentTab, setCurrentTab] = useState(0);
-  // const handleChange = (event, newValue) => {
-  //   setCurrentTab(newValue);
-  // };
 
   const TABs = tabItems.map((tab, index) => (
     <Tab
