d48b0a6a7fed6674f70ad756b550a778a94afa3b
Ankerso <ankersosed@nci-02277705-ml.nih.gov>
2023-06-15 12:26:43 -0400

    Handled undefined cases

--- packages/query-bar/src/generators/QueryBarGenerator.js
+++ packages/query-bar/src/generators/QueryBarGenerator.js
@@ -48,6 +48,8 @@ export const QueryBarGenerator = (uiConfig = DEFAULT_CONFIG) => {
     ? functions.resetFacetSlider
     : DEFAULT_CONFIG.functions.resetFacetSlider;
 
+  const clsName = (text='filter-section') => text.replace(/\s+/g, '-').toLowerCase();
+
   return {
     QueryBar: withStyles(DEFAULT_STYLES, { withTheme: true })((props) => {
       const { statusReducer, localFind, classes } = props;
@@ -163,7 +165,7 @@ export const QueryBarGenerator = (uiConfig = DEFAULT_CONFIG) => {
               ? <span className={classes.operators}> AND </span>
               : null}
             {mappedInputs.map((filter, index) => (
-              <span className={filter.section.replace(/\s+/g, '-').toLowerCase()}>
+              <span className={clsName(filter.section)}>
                 <Filter
                   index={index}
                   type={filter.type}
