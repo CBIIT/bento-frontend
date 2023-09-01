3d5c6bad6bda42ca5bbbc2f73698f072d7a97a01
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-09-01 14:23:24 -0400

    Added label for Checkbox

--- packages/table/src/body/components/CheckBoxView.js
+++ packages/table/src/body/components/CheckBoxView.js
@@ -4,12 +4,15 @@ import {
   TableCell,
 } from '@material-ui/core';
 
+const label = { inputProps: { 'aria-label': 'Checkbox item' } };
+
 const CheckboxView = ({
   row,
   onRowSelectChange,
 }) => (
   <TableCell padding="checkbox">
     <Checkbox
+      {...label}
       disableRipple
       onClick={(event) => onRowSelectChange(event, row)}
       checked={row.isChecked}
