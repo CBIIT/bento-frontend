dfae6ec01f05a50b6e82deadefdff00fc726321b
Ruoxi Zhang <zhang.ruoxi@nih.gov>
2023-09-01 12:24:54 -0400

    Update facet-filter version 1.0.1-ccdihub.19

--- packages/facet-filter/src/components/inputs/slider/InputMinMaxStyle.js
+++ packages/facet-filter/src/components/inputs/slider/InputMinMaxStyle.js
@@ -1,4 +1,16 @@
 export default () => ({
   slider_INPUT_MIN: {
+    fontFamily: 'Montserrat',
+    fontSize: '13px',
+    fontWeight: 500,
+    color: '#717171',
+    background: '#F0F0F0',
+  },
+  slider_INPUT_MAX: {
+    fontFamily: 'Montserrat',
+    fontSize: '13px',
+    fontWeight: 500,
+    color: '#717171',
+    background: '#F0F0F0',
   },
 });
