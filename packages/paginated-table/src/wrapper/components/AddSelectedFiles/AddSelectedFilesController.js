010da9d27f657309922beb5c4926eeda6d7e42de
huuaho <32457341+huuaho@users.noreply.github.com>
2023-11-02 12:47:24 -0400

    cart checks current count before new file for limit

--- packages/paginated-table/src/wrapper/components/AddSelectedFiles/AddSelectedFilesController.js
+++ packages/paginated-table/src/wrapper/components/AddSelectedFiles/AddSelectedFilesController.js
@@ -47,10 +47,15 @@ const AddSelectedFilesController = (props) => {
       fileIds: selectedRows,
       query: addFileQuery,
     });
+    let existingIdCount = 0;
+    if (localStorage.getItem('CartFileIds')) {
+      const filesId = JSON.parse(localStorage.getItem('CartFileIds')) || [];
+      existingIdCount = filesId.length;
+    }
 
     fileIds().then((response) => {
       const ids = addFilesResponseHandler(response, responseKeys);
-      if (ids.length >= maxFileLimit) {
+      if (ids.length + existingIdCount >= maxFileLimit) {
         setAlterDisplay(true);
       } else {
         addFiles(ids);
