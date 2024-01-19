9be8b5ddae0b67657b63af131add205a5d2100b9
Udosen <udosent2@nih.gov>
2024-01-19 09:50:14 -0600

    remove HashRouter

--- packages/util/src/RouteLinks.js
+++ packages/util/src/RouteLinks.js
@@ -11,26 +11,17 @@ function linkIsEmail(linkElement) {
   return /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}/.test(linkElement);
 }
 
-const RouteLinks = ({ title, to, children }) => {
-  console.log('RouteLinks', {
-    title,
-    to,
-    children,
-    isLinkExternal: linkIsInternal(to),
-  });
-
-  return linkIsEmail(to) ? (
-    <a href={`mailto:${to}`}>{children}</a>
-  ) : linkIsInternal(to) ? (
-    <Link to={to} title={title}>
-      {children}
-    </Link>
-  ) : (
-    <a href={to} target="external-url">
-      {children}
-    </a>
-  );
-};
+const RouteLinks = ({ title, to, children }) => (linkIsEmail(to) ? (
+  <a href={`mailto:${to}`}>{children}</a>
+) : linkIsInternal(to) ? (
+  <Link to={to} title={title}>
+    {children}
+  </Link>
+) : (
+  <a href={to} target="external-url">
+    {children}
+  </a>
+));
 
 RouteLinks.propTypes = {
   to: PropTypes.string.isRequired,
