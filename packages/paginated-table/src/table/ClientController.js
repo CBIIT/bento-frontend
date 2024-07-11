4034b8e88ff241aa8c55f6ccbbdef573e8d4c430
rana22 <ambar.rana123@gmail.com>
2024-07-11 15:13:27 -0400

    row grouping

--- packages/paginated-table/src/table/ClientController.js
+++ packages/paginated-table/src/table/ClientController.js
@@ -25,14 +25,8 @@ const TableController = ((props) => {
     rowsPerPage = 10,
     sortBy,
     sortOrder,
+    groupBy,
   } = table;
-
-  const arraySort = (a, b) => {
-    // handles sort for the column with the array value
-    const aHasData = a[sortBy] && a[sortBy].length > 0;
-    const bHasData = b[sortBy] && b[sortBy].length > 0;
-    return aHasData === bHasData ? 0 : aHasData ? -1 : 1;
-  };
 
   /**
   * client table aplhanumeric sorting for column table
@@ -44,6 +38,64 @@ const TableController = ((props) => {
       sensitivity: 'base',
     },
   );
+
+  // use group index to customize cell view
+  const groupingIndex = (array) => {
+    if (array.length === 0) {
+      return array;
+    }
+    const columnValues = array.map((item) => item[groupBy]);
+    const distinctGroups = [...new Set(columnValues)];
+    const indexedArray = [];
+    distinctGroups.forEach((group) => {
+      let index = 0;
+      array.forEach((row) => {
+        if (row[groupBy] === group) {
+          indexedArray.push({
+            ...row,
+            groupIndex: index,
+          });
+          index += 1;
+        }
+      });
+    });
+    return indexedArray;
+  };
+
+  // sort order with row group
+  // 1. sort by groupBy
+  // 2. sort by current sort column
+  const sortWithRowGrouping = (array = []) => {
+    const sorted = array.sort((a, b) => {
+      // if current sort column is not groupBy column
+      // apply sorting with row grouping
+      if (groupBy !== sortBy) {
+        const rowGrouping = `${a[groupBy]}`.localeCompare(
+          `${b[groupBy]}`, undefined, {
+            numeric: true,
+            sensitivity: 'base',
+          },
+        );
+        if (rowGrouping !== 0) {
+          return rowGrouping;
+        }
+      }
+
+      if (sortOrder === 'asc') {
+        return alphaNumericSort(a, b);
+      }
+      return alphaNumericSort(b, a);
+    });
+
+    return groupingIndex(sorted);
+  };
+
+  const arraySort = (a, b) => {
+    // handles sort for the column with the array value
+    const aHasData = a[sortBy] && a[sortBy].length > 0;
+    const bHasData = b[sortBy] && b[sortBy].length > 0;
+    return aHasData === bHasData ? 0 : aHasData ? -1 : 1;
+  };
 
   /**
   * sort base on table state
@@ -62,7 +114,11 @@ const TableController = ((props) => {
     }
     return alphaNumericSort(b, a);
   };
-  const sortedRows = [...updateRows].sort((a, b) => sortRows(a, b));
+
+  // sort based on row grouping
+  // groupBy column name
+  const sortedRows = (groupBy) ? sortWithRowGrouping(updateRows)
+    : [...updateRows].sort((a, b) => sortRows(a, b));
   const startIndex = page * rowsPerPage;
   const endIndex = startIndex + rowsPerPage;
   const displayRows = [...sortedRows].slice(startIndex, endIndex);
