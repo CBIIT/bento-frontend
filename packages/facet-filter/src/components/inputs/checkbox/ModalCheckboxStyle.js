9b470a11b8dfd0422a8e4db355c5081ca54084b7
Ben Chen <benchen4998@gmail.com>
2024-11-06 16:25:19 -0500

    Update modal styling

---
+++ packages/facet-filter/src/components/inputs/checkbox/ModalCheckboxStyle.js
@@ -0,0 +1,36 @@
+export default () => ({
+  listItemGutters: {
+    padding: '10px 10px 10px 0px',
+    width: '33%',
+    borderRight: '0.5px solid #000000',
+    borderBottom: '0.5px solid #000000',
+  },
+  checkboxRoot: {
+    marginLeft: '8px',
+    height: 12,
+  },
+  panelDetailText: {
+    color: '#323232',
+    fontFamily: 'Nunito',
+    fontSize: '14px',
+    fontWeight: '200',
+  },
+  panelSubjectText: {
+    color: '#323232',
+    fontFamily: 'Nunito',
+    fontSize: '14px',
+    marginRight: '0px',
+  },
+  checkboxLabel: {
+    margin: '0',
+  },
+  checkboxName: {
+    margin: '0',
+    color: '#000000',
+    fontSize: '16px',
+    fontWeight: 300,
+    marginTop: '1.5px',
+    fontFamily: 'Nunito',
+    lineHeight: '120%',
+  },
+});
---
+++ packages/facet-filter/src/components/inputs/checkbox/ModalCheckboxView.js
@@ -0,0 +1,149 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-wrap-multilines */
+/* eslint-disable react/jsx-indent */
+/* eslint-disable object-curly-newline */
+/* eslint-disable object-shorthand */
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import {
+  Checkbox,
+  ListItem,
+  ListItemText,
+  Divider,
+  Tooltip,
+  Box,
+  Typography,
+  Icon,
+} from '@material-ui/core';
+import clsx from 'clsx';
+import {
+  CheckBox as CheckBoxIcon, CheckBoxOutlineBlank as CheckBoxBlankIcon,
+} from '@material-ui/icons';
+import emptyCheckBox from './assets/emptycheckbox.svg';
+import checkedBox from './assets/checkedbox.svg';
+import styles from './ModalCheckboxStyle';
+
+const alignment = 'flex-start';
+
+const CheckBoxView = ({
+  classes,
+  checkboxItem,
+  datafield,
+  onToggle,
+  facet,
+}) => {
+  const {
+    name,
+    subjects,
+    isChecked = false,
+    index,
+    section,
+    tooltip,
+    label,
+  } = checkboxItem;
+  const indexType = index % 2 === 0 ? 'Even' : 'Odd';
+  const checkedSection = `${section}`.toLowerCase().replace(' ', '_');
+
+  const labelCheckbox = { inputProps: { 'aria-label': 'Checkbox item' } };
+
+  const handleToggle = () => {
+    const toggleCheckBoxItem = {
+      name: name,
+      datafield: datafield,
+      isChecked: !isChecked,
+    };
+    onToggle(toggleCheckBoxItem);
+  };
+
+  const LabelComponent = () => (
+    <Box
+      component="div"
+      className={clsx(classes.panelDetailText, {
+        [`${checkedSection}NameUnChecked`]: !isChecked,
+        [`${checkedSection}NameChecked`]: isChecked,
+      })}
+    >
+      {label ? (
+        <Typography className={classes.checkboxLabel}>{label}</Typography>
+      ) : (<Typography className={classes.checkboxName}>{name}</Typography>)}
+    </Box>
+  );
+
+  return (
+    <>
+      <ListItem
+        width={1}
+        button
+        alignItems="flex-start"
+        onClick={handleToggle}
+        classes={{ gutters: classes.listItemGutters }}
+        className={clsx({ [`${checkedSection}Checked${indexType}`]: isChecked })}
+      >
+        <Checkbox
+          id={`checkbox_${facet.label}_${name}`}
+          {...labelCheckbox}
+          icon={
+            <Icon style={{
+              fontSize: '18px',
+              lineHeight: '22px',
+            }}
+            >
+              <img
+                src={emptyCheckBox}
+                alt="checkbox-icon"
+              />
+            </Icon>
+          }
+          onClick={handleToggle}
+          checked={isChecked}
+          checkedIcon={(
+            <Icon
+              style={{
+                fontSize: '18px',
+                lineHeight: '22px',
+              }}
+              className={`${checkedSection}CheckedIcon`}
+            >
+              <img
+                src={checkedBox}
+                alt="checkedBox-icon"
+              />
+            </Icon>
+          )}
+          disableRipple
+          color="secondary"
+          classes={{ root: classes.checkboxRoot }}
+        />
+        { tooltip ? (
+          <Tooltip id={datafield} title={tooltip}>
+            <div className={datafield}>
+              <LabelComponent />
+            </div>
+          </Tooltip>
+        ) : (
+          <LabelComponent />
+        )}
+        <ListItemText />
+        <Typography
+          className={clsx(`${checkedSection}Subjects`, {
+            [`${checkedSection}SubjectUnChecked`]: !isChecked,
+            [`${checkedSection}SubjectChecked`]: isChecked,
+          })}
+        >
+          {`(${subjects})`}
+        </Typography>
+      </ListItem>
+      <Divider
+        style={{
+          backgroundColor: isChecked ? '#FFFFFF' : '#b1b1b1',
+          margin: '0px',
+          height: isChecked ? '2px' : '1px',
+        }}
+      />
+    </>
+  );
+};
+
+export default withStyles(styles)(CheckBoxView);
