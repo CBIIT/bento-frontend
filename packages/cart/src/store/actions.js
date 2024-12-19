4a261add927d089e54bfefcc122d6382852c987e
David-YuWei <ywwei85@gmail.com>
2024-12-19 15:56:03 -0500

    Updating cart use Indexeddb instead of localstorage

--- packages/cart/src/store/actions.js
+++ packages/cart/src/store/actions.js
@@ -1,3 +1,108 @@
+const DB_NAME = 'ccdi-hub-indexeddb-cart-files';
+const DB_STORE_NAME = 'CartStore';
+
+const getCartItem = () => (new Promise((resolve, reject) => {
+  const open = indexedDB.open(DB_NAME, 1);
+  open.onsuccess = () => {
+    const db = open.result;
+    const transaction = db.transaction(DB_STORE_NAME, 'readwrite');
+    const store = transaction.objectStore(DB_STORE_NAME);
+    const request = store.getAll();
+    request.onsuccess = function (event) {
+      resolve(event.target.result);
+    };
+
+    request.onerror = (event) => { reject(event); };
+
+    // Close the db when the transaction is done
+    transaction.oncomplete = () => {
+      db.close();
+    };
+    transaction.onerror = function (event) { reject(event); };
+  };
+  open.onerror = (event) => { reject(event); };
+  open.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    db.createObjectStore(DB_STORE_NAME, { keyPath: 'fileId', autoIncrement: false });
+  };
+}));
+
+const addCartItem = (files) => (new Promise((resolve, reject) => {
+  const open = indexedDB.open(DB_NAME, 1);
+  open.onsuccess = () => {
+    const db = open.result;
+    const transaction = db.transaction(DB_STORE_NAME, 'readwrite');
+    const itemStore = transaction.objectStore(DB_STORE_NAME);
+
+    files.forEach((file) => {
+      itemStore.put({ fileId: file });
+    });
+
+    // Close the db when the transaction is done
+    transaction.oncomplete = () => {
+      db.close();
+    };
+    transaction.onerror = (event) => { reject(event); };
+  };
+  open.onerror = function (event) { reject(event); };
+  open.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    db.createObjectStore(DB_STORE_NAME, { keyPath: 'fileId', autoIncrement: false });
+  };
+}));
+
+const deleteCartItem = (fileId) => (new Promise((resolve, reject) => {
+  const open = indexedDB.open(DB_NAME, 1);
+  open.onsuccess = function () {
+    const db = open.result;
+    const transaction = db.transaction(DB_STORE_NAME, 'readwrite');
+    const store = transaction.objectStore(DB_STORE_NAME);
+    const request = store.delete(fileId);
+    request.onsuccess = () => {
+      resolve();
+    };
+
+    request.onerror = (event) => { reject(event); };
+
+    // Close the db when the transaction is done
+    transaction.oncomplete = () => {
+      db.close();
+    };
+    transaction.onerror = function (event) { reject(event); };
+  };
+  open.onerror = function (event) { reject(event); };
+  open.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    db.createObjectStore(DB_STORE_NAME, { keyPath: 'fileId', autoIncrement: false });
+  };
+}));
+
+const deleteAllCartItem = () => (new Promise((resolve, reject) => {
+  const open = indexedDB.open(DB_NAME, 1);
+  open.onsuccess = function () {
+    const db = open.result;
+    const transaction = db.transaction(DB_STORE_NAME, 'readwrite');
+    const store = transaction.objectStore(DB_STORE_NAME);
+    const req = store.clear();
+    req.onsuccess = () => {
+      resolve();
+    };
+    req.onerror = (evt) => {
+      reject(evt);
+    };
+    // Close the db when the transaction is done
+    transaction.oncomplete = function () {
+      db.close();
+    };
+    transaction.onerror = function (event) { reject(event); };
+  };
+  open.onerror = function (event) { reject(event); };
+  open.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    db.createObjectStore(DB_STORE_NAME, { keyPath: 'fileId', autoIncrement: false });
+  };
+}));
+
 export const actionTypes = {
   INIT_CART: 'INIT_CART',
   ADD_CART_FILES: 'ADD_CART_FILES',
@@ -5,20 +110,58 @@ export const actionTypes = {
   DELETE_ALL_CART_FILES: 'DELETE_ALL_CART_FILES',
 };
 
-export const initCart = () => ({
+// export const initCart = () => ({
+//   type: actionTypes.INIT_CART,
+// });
+
+export const onInitCartRedux = (data) => ({
   type: actionTypes.INIT_CART,
+  payload: data,
 });
 
-export const onAddCartFiles = (files) => ({
+// Async action with redux-thunk
+export const initCart = () => ((dispatch) => {
+  getCartItem()
+    .then((data) => {
+      dispatch(onInitCartRedux(data));
+    });
+});
+
+export const onAddCartFilesRedux = (files) => ({
   type: actionTypes.ADD_CART_FILES,
   payload: files,
 });
 
-export const onDeleteCartFile = (fileId) => ({
+// Async action with redux-thunk
+export const onAddCartFiles = (files) => ((dispatch) => {
+  dispatch(onAddCartFilesRedux(files));
+  addCartItem(files)
+    .then(() => {
+      // dispatch(onAddCartFilesRedux(files));
+    });
+});
+
+export const onDeleteCartFileRedux = (fileId) => ({
   type: actionTypes.DELETE_CART_FILE,
   payload: fileId,
 });
 
-export const onDeleteAllCartFile = () => ({
+// Async action with redux-thunk
+export const onDeleteCartFile = (fileId) => ((dispatch) => {
+  deleteCartItem(fileId)
+    .then(() => {
+      dispatch(onDeleteCartFileRedux(fileId));
+    });
+});
+
+export const onDeleteAllCartFileRudex = () => ({
   type: actionTypes.DELETE_ALL_CART_FILES,
+});
+
+// Async action with redux-thunk
+export const onDeleteAllCartFile = () => ((dispatch) => {
+  deleteAllCartItem()
+    .then(() => {
+      dispatch(onDeleteAllCartFileRudex());
+    });
 });
--- packages/cart/src/store/reducers.js
+++ packages/cart/src/store/reducers.js
@@ -5,11 +5,13 @@ import { actionTypes } from './actions';
 * 1. files ids in cart are stored on localstore
 * 2. clear cache will remove all the files from cart
 */
-const initCartState = () => {
-  if (localStorage.getItem('CartFileIds')) {
-    const initState = {};
-    initState.filesId = JSON.parse(localStorage.getItem('CartFileIds')) || [];
-    return initState;
+const initCartState = {
+  filesId: [],
+};
+
+const onInitCartState = (state, data) => {
+  if (data) {
+    return { filesId: data.map((dt) => dt.fileId) };
   }
   return {
     filesId: [],
@@ -30,7 +32,7 @@ const addFilesToCart = (state, payload = []) => {
         state.filesId.concat(payload),
       ),
     ) : state.filesId;
-  localStorage.setItem('CartFileIds', JSON.stringify(uniqueFileIds) || []);
+  // localStorage.setItem('CartFileIds', JSON.stringify(uniqueFileIds) || []);
   return {
     filesId: [...uniqueFileIds],
     count: distinctIds.length,
@@ -40,16 +42,21 @@ const addFilesToCart = (state, payload = []) => {
 const getFilesAfterDel = (state, payload) => {
   const { filesId } = state;
   const fileIdsAfterDeletion = filesId.filter((file) => file !== payload);
-  localStorage.setItem('CartFileIds', JSON.stringify(fileIdsAfterDeletion));
+  // localStorage.setItem('CartFileIds', JSON.stringify(fileIdsAfterDeletion));
   return {
     filesId: fileIdsAfterDeletion,
     count: fileIdsAfterDeletion.length,
   };
 };
 
-export const cartReducer = (state = initCartState(), action) => {
+export const cartReducer = (state = initCartState, action) => {
   const { payload, type } = action;
   switch (type) {
+    case actionTypes.INIT_CART:
+      return {
+        ...state,
+        ...onInitCartState(state, payload),
+      };
     case actionTypes.ADD_CART_FILES:
       return {
         ...state,
@@ -62,7 +69,7 @@ export const cartReducer = (state = initCartState(), action) => {
       };
     case actionTypes.DELETE_ALL_CART_FILES:
       // clear all local storage if we remove all record
-      localStorage.clear();
+      // localStorage.clear();
       return {
         ...state,
         filesId: [],
