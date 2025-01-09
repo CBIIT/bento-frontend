705ffd6907fac900857638e9259c2f4f95ccfb41
rana22 <ambar.rana123@gmail.com>
2025-01-09 16:20:17 -0500

    revert code

--- packages/navigator/src/components/Navigator/components/PDF/portrait/util.js
+++ packages/navigator/src/components/Navigator/components/PDF/portrait/util.js
@@ -41,3 +41,21 @@ export const formatEnumValues = (enums) => {
   }
   return JSON.stringify(enums);
 };
+
+// Capitalize first letter of each word
+export const capitalizeFirstLetter = (str) => {
+  if (str.toLowerCase() === 'non-member') {
+    return 'Non-Member';
+  }
+  if (str.toLowerCase() === 'nih') {
+    return 'NIH';
+  }
+  if (str.toLowerCase() === 'esi') {
+    return 'ESI';
+  }
+  if (str.toLowerCase() === 'google') {
+    return 'Google';
+  }
+  const words = str.split(' ');
+  return words.map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join(' ');
+};
