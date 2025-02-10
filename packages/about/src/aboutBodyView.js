8015171e17670e50021e853c4966ed3d14d6036d
Alejandro-Vega <vegaalejandro15@gmail.com>
2025-02-10 11:44:39 -0500

    Allow the passing of a title attribute to links in the about pages

--- packages/about/src/aboutBodyView.js
+++ packages/about/src/aboutBodyView.js
@@ -70,8 +70,10 @@ const AboutBody = ({
                       { contentObj.paragraph.split('$$').map((splitedParagraph) => {
                         // Checking for regex ()[] pattern
                         if (splitedParagraph != null && ((/\[(.+)\]\((.+)\)/g.test(splitedParagraph)) || (/\((.+)\)\[(.+)\]/g.test(splitedParagraph)))) {
-                          const title = splitedParagraph.match(/\[(.*)\]/).pop();
+                          const defaultTitle = splitedParagraph.match(/\[(.*)\]/).pop();
                           const linkAttrs = splitedParagraph.match(/\((.*)\)/).pop().split(' ');
+                          const titleMatch = splitedParagraph.match(/title:\s*'([^']+)'/);
+                          const title = titleMatch ? titleMatch[1].trim() : defaultTitle;
                           const target = linkAttrs.find((link) => link.includes('target:'));
                           const url = linkAttrs.find((link) => link.includes('url:'));
                           const href = splitedParagraph.match(/\((.*)\)/).pop();
@@ -85,7 +87,7 @@ const AboutBody = ({
                               color="inherit"
                               className={classes.link}
                             >
-                              {title}
+                              {defaultTitle}
                             </Link>
                           );
 
