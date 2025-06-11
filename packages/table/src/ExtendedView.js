eeb82d420c4d67afe75ccb7fd57188175a4edfe5
Ben Chen <benchen4998@gmail.com>
2025-06-11 09:07:20 -0400

    PORTAL-2105: Add option for participant id search box

--- packages/table/src/ExtendedView.js
+++ packages/table/src/ExtendedView.js
@@ -44,36 +44,41 @@ const ExtendedView = ({
       </Toolbar>
       )} */}
       {
-          (pagination) && (
-            <div className="downloadArea" style={downloadAreaStyle}>
-              <CustomPagination
-                customTheme={customTheme.tblTopPgn}
-                rowsPerPageOptions={[10, 25, 50, 100]}
-                component="div"
+        (pagination) && (
+          <div className="downloadArea" style={downloadAreaStyle}>
+            {
+              (table.showSearchBox) && (
+                <table.SearchBox />
+              )
+            }
+            <CustomPagination
+              customTheme={customTheme.tblTopPgn}
+              rowsPerPageOptions={[10, 25, 50, 100]}
+              component="div"
+              count={table.totalRowCount || 0}
+              rowsPerPage={table.rowsPerPage || 10}
+              page={table.page || 0}
+              onPageChange={onPageChange}
+              onRowsPerPageChange={onRowsPerPageChange}
+              queryVariables={queryVariables}
+              table={table}
+            />
+            <ManageColumnView
+              table={table}
+              manageViewColumns={manageViewColumns}
+              onColumnViewChange={onColumnViewChange}
+              onAllColumnViewChange={onAllColumnViewChange}
+            />
+            {hasExport && (
+              <DownloadButton
                 count={table.totalRowCount || 0}
-                rowsPerPage={table.rowsPerPage || 10}
-                page={table.page || 0}
-                onPageChange={onPageChange}
-                onRowsPerPageChange={onRowsPerPageChange}
                 queryVariables={queryVariables}
                 table={table}
               />
-              <ManageColumnView
-                table={table}
-                manageViewColumns={manageViewColumns}
-                onColumnViewChange={onColumnViewChange}
-                onAllColumnViewChange={onAllColumnViewChange}
-              />
-              {hasExport && (
-                <DownloadButton
-                  count={table.totalRowCount || 0}
-                  queryVariables={queryVariables}
-                  table={table}
-                />
-              )}
-            </div>
-          )
-        }
+            )}
+          </div>
+        )
+      }
     </ThemeProvider>
   );
 };
