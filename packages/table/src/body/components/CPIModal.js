8c32f9e10c38cbaeb3b813461ed488df75e75cdd
Ankerso <ankersosed@nci-02346685-ml.nih.gov>
2025-09-18 15:37:48 -0400

    Fixed CPi

--- packages/table/src/body/components/CPIModal.js
+++ packages/table/src/body/components/CPIModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Box,
   Modal,
@@ -44,10 +44,12 @@ const CPIModal = ({
 }) => {
   const [sortBy, setSortBy] = useState('associated_id');
   const [sortOrder, setSortOrder] = useState('asc');
-  const [data, setData] = useState(() => {
-    const result = row.cpi_data;
-    return result;
-  });
+  const [data, setData] = useState(row.cpi_data);
+
+  useEffect(() => {
+    setData(row.cpi_data);
+  }, [row]);
+
 
   const sortingData = (column, newOrder) => {
     const sortedData = data.sort((a, b) => a[column].localeCompare(b[column]));
