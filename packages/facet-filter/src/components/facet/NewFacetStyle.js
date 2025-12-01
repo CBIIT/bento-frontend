97029409d304b6d4c9fd404ad311ffee064d8203
huuaho <32457341+huuaho@users.noreply.github.com>
2025-11-30 22:06:48 -0500

    added new components

---
+++ packages/facet-filter/src/components/facet/NewFacetStyle.js
@@ -0,0 +1,219 @@
+export default () => ({
+  expansionPanelDetailsRoot: {
+    display: 'block',
+  },
+  expansionPanelsideBarItem: {
+    boxShadow: 'none',
+    marginTop: '8px',
+    margin: 'auto',
+    position: 'initial',
+    '&:before': {
+      position: 'initial',
+    },
+  },
+  subSectionSummaryText: {
+    marginLeft: '10px',
+    color: '#323232',
+    fontFamily: 'Raleway',
+    fontSize: '13px',
+    fontWeight: 'bold',
+    letterSpacing: '0.25px',
+  },
+  sortGroup: {
+    paddingTop: '10px',
+    marginBottom: '5px',
+    textAlign: 'left',
+    marginLeft: '-5px',
+    borderTop: 'none',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  sortGroupIcon: {
+    cursor: 'pointer',
+    fontFamily: 'Nunito',
+    fontSize: '10px',
+    marginRight: '12px',
+    marginLeft: '24px',
+  },
+  sortGroupItem: {
+    cursor: 'pointer',
+    fontFamily: 'Nunito',
+    fontSize: '10px',
+    marginRight: '32px',
+  },
+  NonSortGroup: {
+    marginBottom: '5px',
+    borderTop: '1px solid #B1B1B1',
+    textAlign: 'left',
+    paddingLeft: '10px',
+  },
+  NonSortGroupItem: {
+    fontFamily: 'Nunito',
+    fontSize: '10px',
+    marginRight: '32px',
+  },
+  sortGroupItemCounts: {
+    marginLeft: 'auto',
+    marginRight: '15px',
+    cursor: 'pointer',
+    fontFamily: 'Nunito',
+    fontSize: '10px',
+  },
+  highlight: {
+    color: '#b2c6d6',
+  },
+  showMore: {
+    textAlign: 'right',
+    paddingRight: '5px',
+    cursor: 'pointer',
+    fontSize: '10px',
+    width: '100%',
+  },
+  searchContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    position: 'relative',
+  },
+  searchBox: {
+    fontSize: '14px',
+    fontFamily: 'Nunito',
+    width: 'calc(100% - 26px)',
+    height: '30px',
+    marginTop: '12px',
+    borderRadius: '5px',
+    marginLeft: '6px',
+    border: '1px solid',
+    padding: '5px',
+  },
+  searchBoxWithText: {
+    fontSize: '14px',
+    fontFamily: 'Nunito',
+    width: 'calc(100% - 26px)',
+    height: '30px',
+    marginTop: '12px',
+    borderRadius: '5px',
+    marginLeft: '6px',
+    border: '1px solid',
+    padding: '5px',
+  },
+  expandedDisplayButton: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#4D889E !important',
+    color: 'white',
+    width: 'calc(100% - 26px)',
+    height: '30px',
+    marginBottom: '5px',
+    marginLeft: '6px',
+    padding: '6px 11px',
+    borderRadius: '5px',
+    fontFamily: 'Nunito',
+    fontSize: '11px',
+    fontWeight: '600',
+    top: '8px',
+  },
+  expandedDisplayCount: {
+    marginLeft: '4px',
+    fontFamily: 'Nunito',
+    fontSize: '11px',
+    fontWeight: '600',
+    lineHeight: '17px',
+    color: '#ffffff',
+    backgroundColor: '#5D98AE',
+    padding: '1px 2px',
+  },
+  clearTextButton: {
+    padding: '0px',
+    height: '20px',
+    position: 'absolute',
+    right: '14px',
+    top: '16px',
+  },
+  timeUnitToggle: {
+    marginLeft: '8px',
+    marginRight: '20px',
+    height: '32px',
+    '& .MuiToggleButtonGroup-grouped': {
+      borderRadius: '8px',
+      '&:not(:first-child)': {
+        marginLeft: '0px',
+        borderLeft: '1px solid #D3D3D3',
+        borderTopLeftRadius: '0px',
+        borderBottomLeftRadius: '0px',
+      },
+      '&:not(:last-child)': {
+        borderTopRightRadius: '0px',
+        borderBottomRightRadius: '0px',
+      },
+    },
+  },
+  toggleButton: (props) => {
+    const primaryColor = (props.facet && props.facet.style && props.facet.style.colorPrimary)
+      ? props.facet.style.colorPrimary.color
+      : '#3f51b5';
+
+    return {
+      fontFamily: 'Poppins !important',
+      fontSize: '11px !important',
+      fontWeight: '400 !important',
+      fontStyle: 'normal !important',
+      lineHeight: '100% !important',
+      letterSpacing: '0.02em !important',
+      padding: '6px 24px',
+      textTransform: 'uppercase !important',
+      border: '1px solid #D3D3D3',
+      borderRadius: '8px',
+      backgroundColor: '#EDEDED',
+      color: '#000000',
+      '& .MuiToggleButton-label': {
+        fontFamily: 'Poppins !important',
+        fontSize: '11px !important',
+        fontWeight: '400 !important',
+        lineHeight: '100% !important',
+        letterSpacing: '0.02em !important',
+        textTransform: 'uppercase !important',
+      },
+      '&:hover': {
+        backgroundColor: '#E0E0E0',
+      },
+      '&.Mui-disabled': {
+        backgroundColor: '#F5F5F5',
+        color: '#BDBDBD',
+        border: '1px solid #E0E0E0',
+        cursor: 'not-allowed',
+        '& .MuiToggleButton-label': {
+          color: '#BDBDBD',
+        },
+      },
+      '&.Mui-selected': {
+        backgroundColor: primaryColor,
+        color: '#FFFFFF',
+        '& .MuiToggleButton-label': {
+          fontFamily: 'Poppins !important',
+          fontSize: '11px !important',
+          fontWeight: '400 !important',
+          lineHeight: '100% !important',
+          letterSpacing: '0.02em !important',
+          textTransform: 'uppercase !important',
+        },
+        '&:hover': {
+          backgroundColor: primaryColor,
+        },
+      },
+    };
+  },
+  toggleButtonSelected: (props) => {
+    const primaryColor = (props.facet && props.facet.style && props.facet.style.colorPrimary)
+      ? props.facet.style.colorPrimary.color
+      : '#3f51b5';
+
+    return {
+      backgroundColor: primaryColor,
+      color: '#FFFFFF !important',
+      '&:hover': {
+        backgroundColor: primaryColor,
+      },
+    };
+  },
+});
---
+++ packages/facet-filter/src/components/facet/NewSearchFacetView.js
@@ -0,0 +1,297 @@
+import React, { useState } from 'react';
+import {
+  useLocation,
+  useNavigate,
+} from 'react-router-dom';
+import { generateQueryStr } from '@bento-core/util';
+import {
+  Accordion,
+  List,
+  withStyles,
+  Icon,
+  IconButton,
+  Button,
+} from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import clsx from 'clsx';
+import CustomAccordionSummary from '../summary/AccordionSummaryView';
+import { InputTypes } from '../inputs/Types';
+import styles from './NewFacetStyle';
+import FilterItems from '../inputs/FilterItems';
+import SearchFilterItems from '../inputs/SearchFilterItems';
+import { sortType, sortBySection } from '../../utils/Sort';
+import clearIcon from './assets/clearIcon.svg';
+import ReduxSearchCheckbox from '../inputs/checkbox/ReduxSearchCheckbox';
+import ReduxFacetModal from './ReduxFacetModal';
+
+const searchItems = (items, searchText) => {
+  let matchedItems = [];
+  const keyList = searchText.toUpperCase().split(',').filter((item) => item.trim() !== '');
+  if (keyList.length === 0) {
+    matchedItems = items;
+  } else {
+    for (let i = 0; i < items.length; i += 1) {
+      const item = items[i];
+      for (let j = 0; j < keyList.length; j += 1) {
+        const key = keyList[j].trim();
+        if (key === '' || item.group.toUpperCase().includes(key)) {
+          matchedItems.push(item);
+          break;
+        }
+      }
+    }
+  }
+  return matchedItems;
+};
+
+const SearchFacetView = ({
+  classes,
+  facet,
+  searchText,
+  sortBy,
+  onClearFacetSection,
+  onUnknownAgesChange,
+  onSearchTextChange,
+  onSortChange,
+  CustomView,
+  queryParams,
+  unknownAgesState,
+}) => {
+  const [expand, setExpand] = useState(facet.expanded !== undefined && typeof facet.expanded === 'boolean' ? facet.expanded : false);
+  const [open, setOpen] = useState(false);
+  const onExpandFacet = () => setExpand(!expand);
+  const query = new URLSearchParams(useLocation().search);
+  const navigate = useNavigate();
+
+  const onClearSection = () => {
+    const field = facet.datafield;
+    const paramValue = {};
+    paramValue[field] = '';
+
+    // Also clear the corresponding unknownAges parameter if it exists
+    const unknownAgesField = `${field}_unknownAges`;
+    if (queryParams.includes(unknownAgesField)) {
+      paramValue[unknownAgesField] = '';
+    }
+
+    const queryStr = generateQueryStr(query, queryParams, paramValue);
+    navigate(`/explore${queryStr}`, { replace: true });
+    onSortChange(facet.datafield, null);
+
+    // Reset the corresponding unknownAges parameter in Redux state
+    if (queryParams.includes(unknownAgesField) && onUnknownAgesChange) {
+      onUnknownAgesChange(field, 'include');
+    }
+    onClearFacetSection(facet);
+  };
+
+  /**
+   * display checked items on facet collapse
+   */
+  const {
+    type, facetValues, datafield, section,
+  } = facet;
+  const selectedItems = facetValues && facetValues.filter((item) => item.isChecked);
+  const displayFacet = { ...facet };
+  displayFacet.facetValues = selectedItems;
+
+  // Check if facet is active based on selected items or unknown ages selection
+  const hasSelectedItems = [...selectedItems].length > 0;
+  const unknownAges = unknownAgesState?.[facet.datafield] || 'include';
+  const hasUnknownAgesSelection = unknownAges !== 'include';
+  const isActiveFacet = hasSelectedItems || (type === InputTypes.SLIDER && hasUnknownAgesSelection);
+  const limitCheckBoxCount = facet?.showCheckboxCount || 5;
+
+  const sortFilters = sortBySection({ ...facet, sortBy });
+
+  const checkedItems = sortFilters.filter((item) => item.isChecked)
+    .map((item, index) => (
+      <ReduxSearchCheckbox
+        checkboxItem={{ ...item, index, section }}
+        datafield={datafield}
+        facet={facet}
+        queryParams={queryParams}
+      />
+    ));
+
+  const newUncheckedFullList = searchItems(sortFilters.filter((item) => !item.isChecked),
+    searchText);
+
+  return (
+    <>
+      <ReduxFacetModal
+        facet={facet}
+        queryParams={queryParams}
+        open={open}
+        onClose={() => setOpen(false)}
+      />
+      <Accordion
+        square
+        expanded={expand}
+        onChange={onExpandFacet}
+        classes={{
+          root: classes.expansionPanelsideBarItem,
+        }}
+        id={facet.section}
+      >
+        {CustomView ? (
+          <CustomView
+            facet={facet}
+            facetClasses={
+              isActiveFacet ? `activeFacet${facet.section}`
+                : `inactiveFacet${facet.section}`
+            }
+          />
+        ) : (
+          <CustomAccordionSummary>
+            <div
+              id={
+                `filterGroup_ ${facet.datafield}
+                ${facet.label}`
+              }
+              className={clsx(classes.subSectionSummaryText, {
+                [`activeFacet${facet.section}`]: isActiveFacet,
+              })}
+            >
+              {facet.label}
+            </div>
+          </CustomAccordionSummary>
+        )}
+        {
+          (facetValues.length === 0)
+          && (
+            <div className={classes.NonSortGroup}>
+              <span
+                className={classes.NonSortGroupItem}
+              >
+                No data for this field
+              </span>
+            </div>
+          )
+        }
+        {
+          (facetValues.length > 0)
+          && (
+            <>
+              <div className={classes.searchContainer}>
+                <input
+                  className={searchText && searchText.trim() !== '' ? classes.searchBoxWithText : classes.searchBox}
+                  value={searchText}
+                  type="text"
+                  placeholder={facet.searchPlaceholder ? facet.searchPlaceholder : 'e.g. Sarcoma, Neoplasm'}
+                  onChange={(e) => onSearchTextChange(facet.datafield, e.target.value)}
+                  aria-label={`Search within ${facet.section} facet`}
+                />
+                {
+                  searchText
+                  && searchText.trim() !== ''
+                  && (
+                  <IconButton
+                    aria-label="close"
+                    onClick={() => onSearchTextChange(facet.datafield, '')}
+                    className={classes.clearTextButton}
+                    sx={(theme) => ({
+                      position: 'absolute',
+                      right: 7,
+                      top: 7,
+                      color: theme.palette.grey[500],
+                    })}
+                  >
+                    <CloseIcon fontSize="small" />
+                  </IconButton>
+                  )
+                }
+              </div>
+              {
+                !facet.disableSearchModal && (
+                <div className={classes.searchContainer}>
+                  <Button variant="text" className={classes.expandedDisplayButton} onClick={() => setOpen(!open)}>
+                    VIEW EXPANDED DISPLAY
+                    <span className={classes.expandedDisplayCount}>{`${checkedItems.length + newUncheckedFullList.length}`}</span>
+                  </Button>
+                </div>
+                )
+              }
+            </>
+          )
+        }
+        {
+          (facetValues.length > 0)
+            && (
+              <>
+                <div className={classes.sortGroup}>
+                  <span className={classes.sortGroupIcon}>
+                    <Icon
+                      style={{ fontSize: 10 }}
+                      onClick={onClearSection}
+                    >
+                      <img
+                        src={clearIcon}
+                        height={12}
+                        width={12}
+                        alt="clear-icon"
+                      />
+                    </Icon>
+                  </span>
+                  <span
+                    className={
+                          clsx(classes.sortGroupItem, {
+                            [classes.highlight]: sortBy === sortType.ALPHABET,
+                          })
+                        }
+                    onClick={() => {
+                      onSortChange(facet.datafield, sortType.ALPHABET);
+                    }}
+                  >
+                    Sort Alphabetically
+                  </span>
+                  <span
+                    className={
+                          clsx(classes.sortGroupItemCounts, {
+                            [classes.highlight]: sortBy === sortType.NUMERIC,
+                          })
+                        }
+                    onClick={() => {
+                      onSortChange(facet.datafield, sortType.NUMERIC);
+                    }}
+                  >
+                    Sort by Count
+                  </span>
+                </div>
+                <SearchFilterItems
+                  searchText={searchText}
+                  facet={facet}
+                  queryParams={queryParams}
+                  sortBy={sortBy}
+                />
+              </>
+            )
+        }
+      </Accordion>
+      {
+        (!expand && type === InputTypes.CHECKBOX) && (
+          <>
+            <List id="filter_Items">
+              <FilterItems
+                facet={displayFacet}
+                queryParams={queryParams}
+              />
+            </List>
+          </>
+        )
+      }
+      {
+        (!expand && selectedItems.length > limitCheckBoxCount) && (
+          <div
+            className={classes.showMore}
+            onClick={onExpandFacet}
+          >
+            ...expand to see all selections
+          </div>
+        )
+      }
+    </>
+  );
+};
+
+export default withStyles(styles)(SearchFacetView);
