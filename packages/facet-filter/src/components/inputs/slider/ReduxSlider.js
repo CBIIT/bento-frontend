98ef2a60b395d514ba74e64f423a3890a31f6e63
huuaho <32457341+huuaho@users.noreply.github.com>
2025-12-01 21:47:36 -0500

    added new sliders

--- packages/facet-filter/src/components/inputs/slider/ReduxSlider.js
+++ packages/facet-filter/src/components/inputs/slider/ReduxSlider.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { toggleSilder } from '../../../store/actions/Actions';
+import { toggleSilder, unknownAgesChange } from '../../../store/actions/Actions';
 import SliderView from './SliderView';
 
 const ReduxSlider = ((props) => <SliderView {...props} />);
 
 const mapStateToProps = (state) => ({
   filterState: state.statusReducer.filterState,
+  unknownAgesState: state?.statusReducer?.unknownAgesState || {},
 });
 
 const mapDispatchToProps = (dispatch) => ({
   onSliderToggle: (slider) => dispatch(toggleSilder(slider)),
+  onUnknownAgesChange: (datafield, unknownAges) => (
+    dispatch(unknownAgesChange(datafield, unknownAges))
+  ),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ReduxSlider);
