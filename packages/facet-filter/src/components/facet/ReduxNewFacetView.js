74b56f9f74d01431739c3e432bc4a31486325193
huuaho <32457341+huuaho@users.noreply.github.com>
2026-01-18 18:02:28 -0500

    updated clear facet logic to update URL as well

--- packages/facet-filter/src/components/facet/ReduxNewFacetView.js
+++ packages/facet-filter/src/components/facet/ReduxNewFacetView.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import { generateQueryStr } from '@bento-core/util';
 import {
   clearFacetSection,
   clearSliderSection,
@@ -8,7 +10,52 @@ import {
 } from '../../store/actions/Actions';
 import NewFacetView from './NewFacetView';
 
-const ReduxNewFacetView = ((props) => <NewFacetView {...props} />);
+const ReduxNewFacetView = ((props) => {
+  const query = new URLSearchParams(useLocation().search);
+  const {
+    queryParams, onClearFacetSection, onClearSliderSection, ...restProps
+  } = props;
+
+  const handleClearFacetSection = (facet) => {
+    // Only update URL if updateURL flag is explicitly set to true in facet config
+    if (facet.updateURL === true && queryParams) {
+      const paramValue = {};
+      paramValue[facet.datafield] = '';
+      const queryStr = generateQueryStr(query, queryParams, paramValue);
+      // Use replaceState to update URL without triggering navigation/re-render
+      window.history.replaceState(null, '', `/explore${queryStr}`);
+    }
+    onClearFacetSection(facet);
+  };
+
+  const handleClearSliderSection = (facet) => {
+    // Only update URL if updateURL flag is explicitly set to true in facet config
+    if (facet.updateURL === true && queryParams) {
+      const paramValue = {};
+      paramValue[facet.datafield] = '';
+
+      // Also clear the corresponding unknownAges parameter if it exists
+      const unknownAgesField = `${facet.datafield}_unknownAges`;
+      if (queryParams.includes(unknownAgesField)) {
+        paramValue[unknownAgesField] = '';
+      }
+
+      const queryStr = generateQueryStr(query, queryParams, paramValue);
+      // Use replaceState to update URL without triggering navigation/re-render
+      window.history.replaceState(null, '', `/explore${queryStr}`);
+    }
+    onClearSliderSection(facet);
+  };
+
+  return (
+    <NewFacetView
+      {...restProps}
+      queryParams={queryParams}
+      onClearFacetSection={handleClearFacetSection}
+      onClearSliderSection={handleClearSliderSection}
+    />
+  );
+});
 
 const mapStateToProps = (state) => ({
   timeUnitState: state?.statusReducer?.timeUnitState || {},
