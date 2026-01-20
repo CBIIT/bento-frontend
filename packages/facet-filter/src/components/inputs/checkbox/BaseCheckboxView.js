8f1b3d40c65d3bf9e7517c77f5472ae7734901b3
huuaho <32457341+huuaho@users.noreply.github.com>
2026-01-20 16:46:15 -0500

    removed duplicated line

--- packages/facet-filter/src/components/inputs/checkbox/BaseCheckboxView.js
+++ packages/facet-filter/src/components/inputs/checkbox/BaseCheckboxView.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-wrap-multilines */
 /* eslint-disable react/jsx-indent */
 /* eslint-disable object-curly-newline */
