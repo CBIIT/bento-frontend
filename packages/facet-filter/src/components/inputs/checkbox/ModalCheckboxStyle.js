31b22b34dea3aa674a1e5a5d6c822e079eaed8ff
huuaho <32457341+huuaho@users.noreply.github.com>
2026-01-27 14:40:37 -0500

    comment 294

--- packages/facet-filter/src/components/inputs/checkbox/ModalCheckboxStyle.js
+++ packages/facet-filter/src/components/inputs/checkbox/ModalCheckboxStyle.js
@@ -1,4 +1,4 @@
-export default () => ({
+const defaultStyles = {
   listItemGutters: {
     padding: '10px 10px 10px 0px',
     width: '33.33%',
@@ -33,4 +33,49 @@ export default () => ({
     fontFamily: 'Nunito',
     lineHeight: '120%',
   },
+};
+
+export default () => ({
+  listItemGutters: (props) => ({
+    ...defaultStyles.listItemGutters,
+    ...(props.facet && props.facet.style && props.facet.style.modalCheckbox
+      && props.facet.style.modalCheckbox.listItemGutters
+      ? props.facet.style.modalCheckbox.listItemGutters
+      : {}),
+  }),
+  checkboxRoot: (props) => ({
+    ...defaultStyles.checkboxRoot,
+    ...(props.facet && props.facet.style && props.facet.style.modalCheckbox
+      && props.facet.style.modalCheckbox.checkboxRoot
+      ? props.facet.style.modalCheckbox.checkboxRoot
+      : {}),
+  }),
+  panelDetailText: (props) => ({
+    ...defaultStyles.panelDetailText,
+    ...(props.facet && props.facet.style && props.facet.style.modalCheckbox
+      && props.facet.style.modalCheckbox.panelDetailText
+      ? props.facet.style.modalCheckbox.panelDetailText
+      : {}),
+  }),
+  panelSubjectText: (props) => ({
+    ...defaultStyles.panelSubjectText,
+    ...(props.facet && props.facet.style && props.facet.style.modalCheckbox
+      && props.facet.style.modalCheckbox.panelSubjectText
+      ? props.facet.style.modalCheckbox.panelSubjectText
+      : {}),
+  }),
+  checkboxLabel: (props) => ({
+    ...defaultStyles.checkboxLabel,
+    ...(props.facet && props.facet.style && props.facet.style.modalCheckbox
+      && props.facet.style.modalCheckbox.checkboxLabel
+      ? props.facet.style.modalCheckbox.checkboxLabel
+      : {}),
+  }),
+  checkboxName: (props) => ({
+    ...defaultStyles.checkboxName,
+    ...(props.facet && props.facet.style && props.facet.style.modalCheckbox
+      && props.facet.style.modalCheckbox.checkboxName
+      ? props.facet.style.modalCheckbox.checkboxName
+      : {}),
+  }),
 });
